f85980b7fa87294c328b038d9948ad87fb962674
Israel Ayomikun Asimi <ayamayomide@gmail.com>
2026-09-06 21:54:19 +0100

    Initial setup with Express

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
